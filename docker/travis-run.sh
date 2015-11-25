c6acb712fc8d7f7b5c03e7b885fb413c5ee9af73
daler <dalerr@niddk.nih.gov>
2015-11-25 10:02:04 -0500

    docker and travis test framework

---
+++ docker/travis-run.sh
@@ -0,0 +1,19 @@
+set -e
+set -x
+
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+src=/tmp/trackhub
+mkdir $src
+rm -f $HERE/../.git/shallow
+git clone $HERE/.. $src
+cd $src
+
+# extract version
+VERSION=$(python -c 'exec(open("trackhub/version.py").read());print(version)')
+conda create -y -n trackhub-env python=2 --file requirements.txt --file bioconda-requirements.txt
+set +x; source activate trackhub-env; set -x
+python setup.py clean sdist
+pip install dist/trackhub-${VERSION}.tar.gz
+conda install -y nose
+nosetests trackhub/test/test.py
