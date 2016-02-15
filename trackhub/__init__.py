b37b9358bf7745ebbb1fe19ca8a371d222b9c0ac
mgperry <mgperry32@gmail.com>
2016-02-15 17:15:47 +0000

    Assembly now subclasses Genome

--- trackhub/__init__.py
+++ trackhub/__init__.py
@@ -2,7 +2,7 @@ from hub import Hub
 import helpers
 from genomes_file import GenomesFile
 from genome import Genome
-from new_genome import NewGenome
+from assembly import Assembly
 from trackdb import TrackDb
 from track import Track, SubGroupDefinition, CompositeTrack, \
     ViewTrack, SuperTrack, AggregateTrack
--- trackhub/assembly.py
+++ trackhub/assembly.py
@@ -5,44 +5,28 @@ except ImportError:
     from ordereddict import OrderedDict
 from validate import ValidationError
 from base import HubComponent
+from genome import Genome
 
 
-class NewGenome(HubComponent):
+class Assembly(Genome):
     def __init__(self, genome, twobit_fn, trackdb=None, genome_file_obj=None, genome_info=None):
         """
-        Represents a 2-line genome stanza within a "genomes.txt" file.
+        Represents a genome stanza within a "genomes.txt" file for a non-UCSC genome.
 
         The file itself is represented by a :class:`GenomesFile` object.
         """
         HubComponent.__init__(self)
-        self.genome = genome
+        Genome.__init__(self, genome, trackdb=None, genome_file_obj=None)
         self.twobit_fn = twobit_fn
-        self.trackdb = None
-        if trackdb is not None:
-            self.add_trackdb(trackdb)
-        if genome_file_obj:
-            self.add_parent(genome_file_obj)
         self.genome_info = dict()
         if genome_info:
             self.genome_info.update(genome_info)
-
-    @property
-    def genome_file_obj(self):
-        try:
-            return self.parent
-        except AttributeError:
-            return None
-
-    def add_trackdb(self, trackdb):
-        self.children = []
-        self.add_child(trackdb)
-        self.trackdb = self.children[0]
 
     def __str__(self):
         try:
             self.validate()
         except ValidationError:
-            return "Unconfigured <Genome> object"
+            return "Unconfigured <Assembly> object"
         s = []
         s.append('genome %s' % self.genome)
         trackdb_relpath = os.path.relpath(self.trackdb.local_fn,
@@ -52,21 +36,8 @@ class NewGenome(HubComponent):
         twobit_relpath = os.path.relpath(self.twobit_fn,
                                           start = os.path.dirname(
                                                     self.parent.parent.local_fn)) #local_fn of hub file
-        s.append('twoBitPath %s' % self.twobit_relpath)
+        s.append('twoBitPath %s' % twobit_relpath)
         for k, v in self.genome_info.iteritems():
             s.append(str(k) + ' ' + str(v))
         return '\n'.join(s) + '\n'
 
-    def validate(self):
-        if len(self.children) == 0:
-            raise ValidationError(
-                "No TrackDb objects provided")
-        if self.trackdb is None:
-            raise ValidationError("No TrackDb objects provided")
-
-    def _render(self):
-        """
-        No file is created from a Genome object -- only from its parent
-        GenomesFile object.
-        """
-        pass
