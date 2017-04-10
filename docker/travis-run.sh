c96bd067805612400d8d778e40b48feb58efe7c1
Ryan Dale <dalerr@niddk.nih.gov>
2017-04-10 09:40:41 -0400

    proper conda env creation

--- docker/travis-run.sh
+++ docker/travis-run.sh
@@ -9,10 +9,19 @@ rm -f $HERE/../.git/shallow
 cp -r $HERE/.. $src
 cd $src
 
+CONDA_ENV=trackhub-test-env
+
+conda config --add channels conda-forge
+conda config --add channels defaults
+conda config --add channels r
+conda config --add channels bioconda
+
+conda env list | grep -q $CONDA_ENV && conda env remove -y -n $CONDA_ENV
+
 # extract version
 VERSION=$(python -c 'exec(open("trackhub/version.py").read());print(version)')
-conda create -y -n trackhub-env python=2 --file requirements.txt
-set +x; source activate trackhub-env; set -x
+conda create -y -n $CONDA_ENV python=3 --file requirements.txt --file test-requirements.txt
+set +x; source activate $CONDA_ENV; set -x
 python setup.py clean sdist
 pip install dist/trackhub-${VERSION}.tar.gz
 conda install -y nose
