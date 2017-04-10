6557eb0a3d73e1df54d84e59326920bae08197b9
Ryan Dale <dalerr@niddk.nih.gov>
2017-04-10 10:43:07 -0400

    refactored upload module and test

--- trackhub/upload.py
+++ trackhub/upload.py
@@ -1,139 +1,179 @@
-from __future__ import absolute_import, print_function
-
 import tempfile
 import os
-from fabric.api import local, settings, run, abort, cd, env, hide, puts
-from fabric.contrib.console import confirm
-from fabric.colors import green, yellow
+import shlex
+import subprocess as sp
+import logging
 from . import track
 
+logger = logging.getLogger(__name__)
 
-def upload_file(host, user, local_fn, remote_fn, port=22,
-                rsync_options='-azvrL --progress', run_local=False,
-                symlink=False, symlink_dir='staging'):
-    results = []
-    puts('\n\n' + green(local_fn) + ' -> ' +  yellow(remote_fn))
-    if symlink:
-        symlink_dest = os.path.join(symlink_dir, remote_fn)
-        os.system('mkdir -p %s' % (os.path.dirname(symlink_dest)))
-        remote_dir = os.path.dirname(remote_fn)
-        os.system('ln -sf %s %s' % (os.path.abspath(local_fn), symlink_dest))
-        local_fn = symlink_dest
-
-    if run_local:
-        if not os.path.exists(os.path.dirname(remote_fn)):
-            remote_dir = os.path.dirname(remote_fn)
-            if remote_dir:
-                results.append(local('mkdir -p %s' % remote_dir))
-
-        results.append(local('rsync %(rsync_options)s %(local_fn)s %(remote_fn)s' % locals()))
-        return results
-    env.host_string = host
-    env.user = user
-    env.port = port
-
-    remote_dir = os.path.dirname(remote_fn)
-
-    with settings(warn_only=True):
-        result = run('ls %s' % remote_dir)
-
-    if result.failed:
-        run('mkdir -p %s' % remote_dir)
-
-    rsync_template = \
-        'rsync %(rsync_options)s %(local_fn)s %(user)s@%(host)s:%(remote_fn)s'
-    with settings(warn_only=True):
-        results.append(local(rsync_template % locals()))
-    return results
+RSYNC_OPTIONS = '--progress -rvL'
 
 
-def upload_hub(host, user, hub, port=22, rsync_options='-azvrL --progress',
-               run_local=False, symlink=False, symlink_dir='staging'):
-    kwargs = dict(host=host, user=user, port=port, rsync_options=rsync_options,
-                  run_local=run_local, symlink=symlink, symlink_dir=symlink_dir)
-    print(kwargs)
-    results = []
+def run(cmds, **kwargs):
+    """
+    Wrapper around subprocess.run, with unicode decoding of output.
 
-    # First the hub file:
-    results.extend(
-        upload_file(
-            local_fn=hub.local_fn,
-            remote_fn=hub.remote_fn,
-            **kwargs)
-    )
+    Additional kwargs are passed to subprocess.run.
+    """
 
-    # Then the genomes file:
-    print(hub.genomes_file.local_fn)
-    results.extend(
-        upload_file(
-            local_fn=hub.genomes_file.local_fn,
-            remote_fn=hub.genomes_file.remote_fn,
-            **kwargs)
-    )
+    try:
+        p = sp.run(cmds, stdout=sp.PIPE, stderr=sp.STDOUT, check=True, **kwargs)
+        p.stdout = p.stdout.decode(errors='replace')
+    except sp.CalledProcessError as e:
+        e.stdout = e.stdout.decode(errors='replace')
+        logger.error('COMMAND FAILED: %s', ' '.join(e.cmd))
+        logger.error('STDOUT+STDERR:\n%s', e.stdout)
+        raise e
+    return p
 
-    # Then any associated trackDb:
-    for g in hub.genomes_file.genomes:
-        results.extend(
-            upload_file(
-                local_fn=g.trackdb.local_fn,
-                remote_fn=g.trackdb.remote_fn,
-                **kwargs
-            )
+
+def symlink(target, linkname):
+    """
+    Create a symlink to `target` called `linkname`.
+
+    Converts `target` and `linkname` to absolute paths; creates
+    `dirname(linkname)` if needed.
+    """
+    target = os.path.abspath(target)
+    linkname = os.path.abspath(linkname)
+    if not os.path.exists(target):
+        raise ValueError("target {} not found".format(target))
+
+    link_dir = os.path.dirname(linkname)
+    if not os.path.exists(link_dir):
+        os.makedirs(link_dir)
+
+    run(['ln', '-s', '-f', target, linkname])
+    return linkname
+
+
+def upload(host, user, local_dir, remote_dir,
+                rsync_options=RSYNC_OPTIONS):
+    """
+    Upload a file or directory via rsync.
+
+    Parameters
+    ----------
+    host : str or None
+        If None, omit the host part and just transfer locally
+
+    user : str or None
+        If None, omit the user part
+
+    local_dir : str
+        If a directory, a trailing "/" will be added.
+
+    remote_dir : str
+        If a directory, a trailing "/" will be added.
+    """
+    if user is None:
+        user = ""
+    else:
+        user = user + "@"
+    if host is None or host == 'localhost':
+        host = ""
+    else:
+        host = host + ":"
+
+    if not local_dir.endswith('/'):
+        local_dir = local_dir + '/'
+
+    if not remote_dir.endswith('/'):
+        remote_dir = remote_dir + '/'
+
+    remote_string = '{user}{host}{remote_dir}'.format(**locals())
+    cmds = ['rsync']
+    cmds += shlex.split(rsync_options)
+    cmds += [local_dir, remote_string]
+    run(cmds)
+    return [remote_string]
+
+
+def local_link(local_fn, remote_fn, staging):
+    """
+    Creates a symlink to a local staging area.
+
+    The link name is built from `remote_fn`, but the absolute path is put
+    inside the staging directory.
+
+    Example
+    -------
+
+    If we have the following initial setup::
+
+        cwd="/home/user"
+        local="data/sample1.bw"
+        remote="/hubs/hg19/a.bw"
+        staging="__staging__"
+
+    Then this function does the equivalent of::
+
+        mkdir -p __staging__/hubs/hg19
+        ln -sf \\
+            /home/user/data/sample1.bw \\
+            /home/user/__staging__/hubs/hg19/a.bw
+    """
+    linkname = os.path.join(staging, remote_fn.lstrip(os.path.sep))
+    return symlink(local_fn, linkname)
+
+
+def stage(x, staging):
+    """
+    Stage an object to the `staging` directory.
+
+    If the object is a Track and is one of the types that needs an index file
+    (bam, vcfTabix), then the index file will be staged as well.
+
+    Returns a list of the linknames created.
+    """
+    if not x.remote_fn:
+        raise ValueError("Object does not have a valid `remote_fn` value")
+
+    if not x.local_fn:
+        raise ValueError("Object does not have a valid `local_fn` value")
+
+    linknames = []
+    def _stg(x, ext=''):
+        linknames.append(
+            local_link(x.local_fn + ext, x.remote_fn + ext, staging)
         )
 
-        # and assemblies
-        if hasattr(g, "local_fn"):
-            results.extend(
-                upload_file(
-                    local_fn=g.local_fn,
-                    remote_fn=g.remote_fn,
-                    **kwargs
-                )
-            )
+    _stg(x)
 
-        if getattr(g, "groups", None) != None:
-            results.extend(
-                upload_file(
-                    local_fn=g.groups.local_fn,
-                    remote_fn=g.groups.remote_fn,
-                    **kwargs
-                )
-            )
+    if isinstance(x, track.Track):
+        if x.tracktype == 'bam':
+            stg(x, ext='.bai')
+        if x.tracktype == 'vcfTabix':
+            stg(x, ext='.tbi')
 
-        if hasattr(g, '_html') and g._html:
-            results.extend(
-                upload_file(
-                    local_fn=g._html.local_fn,
-                    remote_fn=g._html.remote_fn,
-                    **kwargs)
-            )
+    return linknames
 
-    # and finally any HTML files:
+
+def stage_hub(hub, staging=None):
+    """
+    Stage a hub by symlinking all its connected files to a local directory.
+    """
+    if staging is None:
+        staging = tempfile.mkdtemp()
+
+    stage(hub, staging)
+    stage(hub.genomes_file, staging)
+    for genome in hub.genomes_file.genomes:
+        stage(genome.genome_file_obj, staging)
     for t, level in hub.leaves(track.CompositeTrack, intermediate=True):
-        print(repr(t))
         if t._html:
-            results.extend(
-                upload_file(
-                    local_fn=t._html.local_fn,
-                    remote_fn=t._html.remote_fn,
-                    **kwargs)
-            )
-    return results
+            stage(t._html, staging)
+
+    for t, level in hub.leaves(track.Track):
+        stage(t, staging)
 
 
-def upload_track(host, user, track, port=22, rsync_options='-azvrL --progress',
-                 run_local=False):
-    kwargs = dict(host=host, user=user, local_fn=track.local_fn,
-                  remote_fn=track.remote_fn, rsync_options=rsync_options,
-                  run_local=run_local)
-    results = upload_file(**kwargs)
-    if track.tracktype == 'bam':
-        kwargs['local_fn'] += '.bai'
-        kwargs['remote_fn'] += '.bai'
-        results.extend(upload_file(**kwargs))
-
-    if track.tracktype == 'vcfTabix':
-        kwargs['local_fn'] += '.tbi'
-        kwargs['remote_fn'] += '.tbi'
-        results.extend(upload_file(**kwargs))
-    return results
+def upload_hub(host, user, hub, port=22, rsync_options=RSYNC_OPTIONS, staging=None):
+    """
+    Stages a hub and then uploads it.
+    """
+    if staging is None:
+        staging = tempfile.mkdtemp()
+    stage_hub(hub, staging=staging)
+    upload(host, user, local_dir=staging, remote_dir='/', rsync_options=rsync_options)
