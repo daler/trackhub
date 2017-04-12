ebd60c4a6b34640294bcdfd27c74ec73fd273a78
daler <dalerr@niddk.nih.gov>
2017-04-12 14:48:22 -0400

    render hub files in a tempdir by default
    avoids cluttering working directory

--- trackhub/__init__.py
+++ trackhub/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import tempfile
 from .hub import Hub
 from . import helpers
 from .genomes_file import GenomesFile
@@ -12,15 +13,39 @@ from .track import Track, SubGroupDefinition, CompositeTrack, \
 from .version import version as __version__
 
 
-def default_hub(hub_name, genome, short_label, long_label, email):
+def default_hub(hub_name, genome, short_label, long_label, email, basedir=None):
     """
     Returns a fully-connected set of hub components using default filenames.
+
+    Parameters
+    ----------
+
+    hub_name : str
+        Name of the hub
+
+    genome : str
+        Assembly name (hg38, dm6, etc)
+
+    short_label, long_label : str
+        Labels for the hub
+
+    email : str
+        Email to include with hub
+
+    basedir : str or None
+        If None, uses a tempdir when rendering the hub files. If str, render
+        the files there.
     """
     hub = Hub(
         hub=hub_name,
         short_label=short_label,
         long_label=long_label,
         email=email)
+
+    if basedir is None:
+        basedir = tempfile.mkdtemp()
+
+    hub.local_dir = basedir
     genome = Genome(genome)
     genomes_file = GenomesFile()
     trackdb = TrackDb()
