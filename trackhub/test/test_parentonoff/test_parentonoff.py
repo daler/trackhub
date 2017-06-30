f991946da2ff1ddc0d9816b3339e7978c2eec9ba
Kapur <kapurka2@nrusca-sld0015.nibr.novartis.net>
2017-06-30 07:51:54 -0400

    Updating parentonoff fork with nosetest compatible test function; travis-run.sh also updated

--- trackhub/test/test_parentonoff/test_parentonoff.py
+++ trackhub/test/test_parentonoff/test_parentonoff.py
@@ -3,68 +3,71 @@ from trackhub.track import *
 import os
 import inspect
 
-hub, genomes_file, genome, trackdb = default_hub( 
-    hub_name = "TestHub", 
-    short_label="TestHub", 
-    long_label="TestHub", 
-    email="karen.kapur@novartis.com", 
-    genome="hg19"
-) 
-## Write files locally; need to be placed in accessible location
-hub.local_fn="hub.txt"
-genomes_file.local_fn="genomes.txt"
+def test_parentonoff():
 
+    hub, genomes_file, genome, trackdb = default_hub( 
+        hub_name = "TestHub", 
+        short_label="TestHub", 
+        long_label="TestHub", 
+        email="karen.kapur@novartis.com", 
+        genome="hg19"
+    ) 
+    ## Write files locally; need to be placed in accessible location
+    hub.local_fn="hub.txt"
+    genomes_file.local_fn="genomes.txt"
 
-
-testcomposite = CompositeTrack( 
-    name="testcomposite",
-    short_label="testlabelshort", 
-    long_label="testlabellong", 
-    tracktype="bigWig", 
-    visibility="full"
-)
-
-viewtrack = ViewTrack(
-    name = "testviewtrack", 
-    view = "view1", 
-    tracktype = "bigWig", 
-    visibility = "full"
-)
-
-trackoff = Track(
-    name = "E003DNase", 
-    url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E003-DNase.pval.signal.bigwig",
-    tracktype="bigWig",
-    short_label="E003DNase",
-    long_label = "E003DNase",
-    visibility = "full",
-    parentonoff = "off"
-    )
-trackon = Track(
-    name = "E004DNase", 
-    url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E004-DNase.pval.signal.bigwig",
-    tracktype="bigWig",
-    short_label="E004DNase",
-    long_label = "E004DNase",
-    visibility = "full",
-    parentonoff = "on"
-    )
-trackdefault = Track(
-    name = "E005DNase", 
-    url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E005-DNase.pval.signal.bigwig",
-    tracktype="bigWig",
-    short_label="E005DNase",
-    long_label = "E005DNase",
-    visibility = "full"
+    testcomposite = CompositeTrack( 
+        name="testcomposite",
+        short_label="testlabelshort", 
+        long_label="testlabellong", 
+        tracktype="bigWig", 
+        visibility="full"
     )
 
+    viewtrack = ViewTrack(
+        name = "testviewtrack", 
+        view = "view1", 
+        tracktype = "bigWig", 
+        visibility = "full"
+    )
 
-viewtrack.add_tracks( trackoff )  
-viewtrack.add_tracks( trackon )  
-viewtrack.add_tracks( trackdefault )  
+    trackoff = Track(
+        name = "E003DNase", 
+        url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E003-DNase.pval.signal.bigwig",
+        tracktype="bigWig",
+        short_label="E003DNase",
+        long_label = "E003DNase",
+        visibility = "full",
+        parentonoff = "off"
+        )
+    trackon = Track(
+        name = "E004DNase", 
+        url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E004-DNase.pval.signal.bigwig",
+        tracktype="bigWig",
+        short_label="E004DNase",
+        long_label = "E004DNase",
+        visibility = "full",
+        parentonoff = "on"
+        )
+    trackdefault = Track(
+        name = "E005DNase", 
+        url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E005-DNase.pval.signal.bigwig",
+        tracktype="bigWig",
+        short_label="E005DNase",
+        long_label = "E005DNase",
+        visibility = "full"
+        )
+
+
+    viewtrack.add_tracks( trackoff )  
+    viewtrack.add_tracks( trackon )  
+    viewtrack.add_tracks( trackdefault )  
         
     
-testcomposite.add_view( viewtrack )
-trackdb.add_tracks( testcomposite )
+    testcomposite.add_view( viewtrack )
+    trackdb.add_tracks( testcomposite )
 
-results = hub.render()
+    results = hub.render()
+
+    assert( open( 'hg19/trackDb.txt').read() == open('expected/hg19/trackDb.txt').read() )
+
