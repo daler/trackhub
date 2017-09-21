75518b32f0dada62a691a8ccb5bb784fac5660fa
Ryan Dale <dalerr@niddk.nih.gov>
2017-09-21 12:07:21 -0400

    fix miniconda download

--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -7,8 +7,8 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 if ! hash conda; then
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86x_64.sh -b -p /tmp/miniconda
+    curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p /tmp/miniconda
     export PATH=/tmp/miniconda/bin:$PATH
     conda config --add channels defaults
     conda config --add channels conda-forge
