c4936f113f45a42871302ee01f09c987d292787f
Ryan Dale <dalerr@niddk.nih.gov>
2017-09-21 12:33:25 -0400

    more .travis.yml

--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+source activate trackhub-test-env
 pytest trackhub/test
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $HERE/run_build.sh && rm -rf $HERE/trackhub-demo $HERE/example_hub
