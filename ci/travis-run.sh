edb79131d3177dfe9fbeb75ac855d92867b3d50a
Ryan Dale <dalerr@niddk.nih.gov>
2017-09-21 12:39:24 -0400

    more .travis.yml

--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
+export PATH=/tmp/miniconda:$PATH
 source activate trackhub-test-env
-pytest trackhub/test
+pytest -v --doctest-modules trackhub/test
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $HERE/run_build.sh && rm -rf $HERE/trackhub-demo $HERE/example_hub
