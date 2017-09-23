3675fbc92faab47ff43c0ad60cd2084737e0e7b7
Ryan Dale <dalerr@niddk.nih.gov>
2017-09-23 15:50:13 -0400

    fix tests

--- trackhub/test/test_parentonoff/test_parentonoff.py
+++ trackhub/test/test_parentonoff/test_parentonoff.py
@@ -1,73 +1,75 @@
-from trackhub import *
-from trackhub.track import *
+import trackhub
 import os
-import inspect
+
 
 def test_parentonoff():
 
-    hub, genomes_file, genome, trackdb = default_hub( 
-        hub_name = "TestHub", 
-        short_label="TestHub", 
-        long_label="TestHub", 
-        email="karen.kapur@novartis.com", 
+    hub, genomes_file, genome, trackdb = trackhub.default_hub(
+        hub_name="TestHub",
+        short_label="TestHub",
+        long_label="TestHub",
+        email="karen.kapur@novartis.com",
         genome="hg19"
-    ) 
-    ## Write files locally; need to be placed in accessible location
-    hub.local_fn="trackhub/test/test_parentonoff/hub.txt"
-    genomes_file.local_fn="trackhub/test/test_parentonoff/genomes.txt"
+    )
 
-    testcomposite = CompositeTrack( 
+    testcomposite = trackhub.track.CompositeTrack(
         name="testcomposite",
-        short_label="testlabelshort", 
-        long_label="testlabellong", 
-        tracktype="bigWig", 
+        short_label="testlabelshort",
+        long_label="testlabellong",
+        tracktype="bigWig",
         visibility="full"
     )
 
-    viewtrack = ViewTrack(
-        name = "testviewtrack", 
-        view = "view1", 
-        tracktype = "bigWig", 
-        visibility = "full"
+    viewtrack = trackhub.track.ViewTrack(
+        name="testviewtrack",
+        view="view1",
+        tracktype="bigWig",
+        visibility="full"
     )
 
-    trackoff = Track(
-        name = "E003DNase", 
-        url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E003-DNase.pval.signal.bigwig",
+    trackoff = trackhub.track.Track(
+        name="E003DNase",
+        url=("http://egg2.wustl.edu/roadmap/data/byFileType/signal/"
+             "consolidated/macs2signal/pval/E003-DNase.pval.signal."
+             "bigwig"),
         tracktype="bigWig",
         short_label="E003DNase",
-        long_label = "E003DNase",
-        visibility = "full",
-        parentonoff = "off"
+        long_label="E003DNase",
+        visibility="full",
+        parentonoff="off"
         )
-    trackon = Track(
-        name = "E004DNase", 
-        url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E004-DNase.pval.signal.bigwig",
+    trackon = trackhub.track.Track(
+        name="E004DNase",
+        url=("http://egg2.wustl.edu/roadmap/data/byFileType/signal/"
+             "consolidated/macs2signal/pval/E004-DNase.pval.signal"
+             ".bigwig"),
         tracktype="bigWig",
         short_label="E004DNase",
-        long_label = "E004DNase",
-        visibility = "full",
-        parentonoff = "on"
+        long_label="E004DNase",
+        visibility="full",
+        parentonoff="on"
         )
-    trackdefault = Track(
-        name = "E005DNase", 
-        url= "http://egg2.wustl.edu/roadmap/data/byFileType/signal/consolidated/macs2signal/pval/E005-DNase.pval.signal.bigwig",
+    trackdefault = trackhub.track.Track(
+        name="E005DNase",
+        url=("http://egg2.wustl.edu/roadmap/data/byFileType/signal/"
+             "consolidated/macs2signal/pval/E005-DNase.pval.signal"
+             ".bigwig"),
         tracktype="bigWig",
         short_label="E005DNase",
-        long_label = "E005DNase",
-        visibility = "full"
+        long_label="E005DNase",
+        visibility="full"
         )
 
+    viewtrack.add_tracks(trackoff)
+    viewtrack.add_tracks(trackon)
+    viewtrack.add_tracks(trackdefault)
 
-    viewtrack.add_tracks( trackoff )  
-    viewtrack.add_tracks( trackon )  
-    viewtrack.add_tracks( trackdefault )  
-        
-    
-    testcomposite.add_view( viewtrack )
-    trackdb.add_tracks( testcomposite )
+    testcomposite.add_view(viewtrack)
+    trackdb.add_tracks(testcomposite)
 
-    results = hub.render()
+    tmpdir, linknames = trackhub.upload.stage_hub(hub)
 
-
-    assert( open( 'trackhub/test/test_parentonoff/hg19/trackDb.txt').read() == open('trackhub/test/test_parentonoff/expected/hg19/trackDb.txt').read() )
+    assert(
+        open(os.path.join(tmpdir, 'hg19/trackDb.txt')).read() ==
+        open('trackhub/test/test_parentonoff/expected/hg19/trackDb.txt').read()
+    )
