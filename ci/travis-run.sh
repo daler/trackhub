57bcc93199f288004e98e2b495a04efb572802dc
Ryan Dale <dalerr@niddk.nih.gov>
2017-12-18 14:24:58 -0500

    typo

--- ci/travis-run.sh
+++ ci/travis-run.sh
@@ -5,6 +5,6 @@ pytest -v --doctest-modules trackhub
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $HERE/run_build.sh && rm -rf $HERE/trackhub-demo $HERE/example_hub
 (
-  cd $HERE/../docs && make doctest
+  cd $HERE/../doc && make doctest
 )
 $HERE/build-docs.sh
