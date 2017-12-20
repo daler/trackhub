2b8fd2f287b257fd890e892a917d642ccc910168
Ryan Dale <dalerr@niddk.nih.gov>
2017-12-20 17:35:45 -0500

    requirements.txt

--- ci/build_example.py
+++ ci/build_example.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
+"""
+It's much easier to write and debug from ReST when syntax highlighting is
+working. Currently it's working with `.. code-block:: python` but not with `..
+testcode::`. Furthermore, I'd like to have the README tested, but the GitHub
+ReST renderer doesn't support `.. testcode::` directive and so we're stuck with
+``..  code-block:: python``.
+
+This script expects a single `.. code-block:: python` directive which appears
+as a "literal_block" to the docutils parser. This block is then executed just
+like a doctest.
+
+"""
+
 import os
 import subprocess as sp
 from docutils.core import publish_doctree
