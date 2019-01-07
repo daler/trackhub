106f1f8a7a9035b370384e47ecab77ac32a34a1a
Ryan Dale <ryan.dale@nih.gov>
2019-01-06 19:55:38 -0500

    hide warning while testing

--- trackhub/test/deprecated_upload_test.py
+++ trackhub/test/deprecated_upload_test.py
@@ -1,5 +1,6 @@
 import os
 from textwrap import dedent
+import warnings
 import pytest
 import tempfile
 from trackhub import upload
@@ -11,33 +12,34 @@ d = data_dir()
 
 class TestUpload(object):
     def setup(self):
-        self.hub = Hub(
-            hub='example_hub',
-            short_label='example hub',
-            long_label='an example hub for testing',
-            email='none@example.com')
-        self.genomes_file = GenomesFile()
-        self.genome = Genome('dm3')
-        self.trackdb = TrackDb()
+        with pytest.warns(DeprecationWarning):
+            self.hub = Hub(
+                hub='example_hub',
+                short_label='example hub',
+                long_label='an example hub for testing',
+                email='none@example.com')
+            self.genomes_file = GenomesFile()
+            self.genome = Genome('dm3')
+            self.trackdb = TrackDb()
 
-        self.tracks = [
-            Track(
-                name='track1',
-                tracktype='bigBed',
-                local_fn=os.path.join(d, 'random-hg38-0.bigBed'),
-                remote_fn='1.bigbed',
-            ),
-            Track(
-                name='track2',
-                tracktype='bigWig',
-                local_fn=os.path.join(d, 'sine-hg38-0.bedgraph.bw'),
-                remote_fn='2.bw',
-            ),
-        ]
-        self.hub.add_genomes_file(self.genomes_file)
-        self.genomes_file.add_genome(self.genome)
-        self.genome.add_trackdb(self.trackdb)
-        self.trackdb.add_tracks(self.tracks)
+            self.tracks = [
+                Track(
+                    name='track1',
+                    tracktype='bigBed',
+                    local_fn=os.path.join(d, 'random-hg38-0.bigBed'),
+                    remote_fn='1.bigbed',
+                ),
+                Track(
+                    name='track2',
+                    tracktype='bigWig',
+                    local_fn=os.path.join(d, 'sine-hg38-0.bedgraph.bw'),
+                    remote_fn='2.bw',
+                ),
+            ]
+            self.hub.add_genomes_file(self.genomes_file)
+            self.genomes_file.add_genome(self.genome)
+            self.genome.add_trackdb(self.trackdb)
+            self.trackdb.add_tracks(self.tracks)
 
     def test_staging(self):
         staging_dir, linknames = upload.stage_hub(self.hub)
