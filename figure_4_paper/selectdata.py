69848b8306f9cfc26a62864bc0f9a36bb44de080
Eva Jason <eva.jason@nih.gov>
2021-05-17 13:43:33 -0400

    select data from encode

--- figure_4_paper/selectdata.py
+++ figure_4_paper/selectdata.py
@@ -7,6 +7,8 @@ import trackhub
 
 df = edf.encode_dataframe('hg19')
 interesting = df.antibody == 'CTCF'
+interesting &= df.dataType == 'ChipSeq'
+
 interesting &= df.cell.isin(['MCF-7','LNCaP'])
 
 interesting &= df.type.isin(['bam','bed','bigWig','narrowPeak'])
@@ -40,14 +42,10 @@ subgroups = [
             'serum_stimulated_media':'serum_stimulated_media',
             'serum_starved_media':'serum_starved_media'
         }),
-    trackhub.SubGroupDefinition(
-        name='replicate',
-        label='recplicate',
-        mapping={
-            'Rep_1':'Rep_1',
-            'Rep_2':'Rep_2'})]
+]
 
 for i in list(m.cell.unique()):
+    df_cell = m[m["cell"]==i]
     supertrack = trackhub.SuperTrack(
         name = i+'_SuperTrack',
     )
@@ -57,7 +55,7 @@ for i in list(m.cell.unique()):
         name = i+'_composite',
         short_label = 'bam, peaks, and signal',
         tracktype = 'bigWig',
-        dimensions = 'dimX=treatment dimY=replicate',
+        dimensions = 'dimX=treatment',
         visibility = 'full',
     )
     supertrack.add_tracks(composite)
@@ -65,26 +63,28 @@ for i in list(m.cell.unique()):
     signal_view = trackhub.ViewTrack(
         name = i+'_signal_view_track',
         view='signal',
+        visibility='full',
         tracktype='bigWig',
         short_label='signal')
+
     composite.add_view(signal_view)
 
     #bws = m[m["type"] == "bigWig"]
-    for fn in m[m["type"] == "bigWig"].filename:
+    for fn in df_cell[df_cell["type"] == "bigWig"].filename:
         track = trackhub.Track(
             name = fn.split(".")[0],
             tracktype ="bigWig",
             subgroups = {#'cell' : row.cell,
-                'treatment' : m.loc[fn].treatment,
-                'rep' :m.loc[fn].replicate}
+                'treatment' : df_cell.loc[fn].treatment,
+                }
             ,
             color = "182,108,30",
             visibility="full",
-            bigDataUrl = m.loc[fn].url
+            bigDataUrl = df_cell.loc[fn].url
         )
         signal_view.add_tracks(track)
-        print(fn)
-    npks = m[m["type"] == "narrowPeak"]
+       # print(fn)
+    #npks = m[m["type"] == "narrowPeak"]
 
     regions_view = trackhub.ViewTrack(
         name = i+'_region_view_track',
@@ -94,31 +94,35 @@ for i in list(m.cell.unique()):
     composite.add_view(regions_view)
 
     #npks = vm[m["type"] == "narrowPeak"]^
-    for fn in m[m["type"] == "narrowPeak"].filename:
+    for fn in df_cell[df_cell["type"] == "narrowPeak"].filename:
         track = trackhub.Track(
             name = fn.split(".")[0],
             tracktype ="bigNarrowPeak",
             subgroups = {#'cell' : row.cell,
-                'treatment' : m.loc[fn].treatment,
-                'rep' : m.loc[fn].replicate}
-            ,
+                'treatment' : df_cell.loc[fn].treatment},
             color = "30,104,182",
             visibility="dense",
-            bigDataUrl = m.loc[fn].url
+            bigDataUrl = df_cell.loc[fn].url
             )
-        signal_view.add_tracks(track)
+        regions_view.add_tracks(track)
 
+    alignment_view = trackhub.ViewTrack(
+        name = i+'_alignment_view_track',
+        view='alignment',
+        tracktype='bam',
+        short_label='alignment')
+    composite.add_view(alignment_view)
     #bams = m[m["type"] == "bam"]
-    for fn in m[m["type"] == "bam"].filename:
+    for fn in df_cell[df_cell["type"] == "bam"].filename:
         track = trackhub.Track(
             name = fn.split(".")[0],
-            tracktype ="bigNarrowPeak",
+            tracktype ="bam",
             subgroups = {
-                'treatment' : m.loc[fn].treatment,
-                'rep' : m.loc[fn].replicate},
-            bigDataUrl = m.loc[fn].url
+                'treatment' : df_cell.loc[fn].treatment,
+                },
+            bigDataUrl = df_cell.loc[fn].url
             )
-        alignement_view.add_tracks(track)
+        alignment_view.add_tracks(track)
 
-trackhub.upload.upload_hub(hub=hub, host='helix.nih.gov', remote_dir='example_hubs/figure4paper')
+trackhub.upload.upload_hub(hub=hub, host='helix.nih.gov', remote_dir='/data/NICHD-core0/datashare/example_hubs/figure4paper/')
 
